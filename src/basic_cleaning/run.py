63b57ad1f5487e63e05d5a1f0ec52718183bf52d
Philoso-Fish <sbielawski@web.de>
2022-02-23 13:40:17 +0100

    Add bugfix for copy call

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,7 +31,7 @@ def go(args):
     df = df[idx].copy()
 
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
-    df = df[idx].copy
+    df = df[idx].copy()
 
     # Convert last_review to datetime
     logger.info("Converting datatypes")
